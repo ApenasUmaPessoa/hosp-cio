c64057dbc445501ef04b58cf43d4247ca7d1b424
ApenasUmaPessoa <antoniolima0508@gmail.com>
2025-09-06 10:34:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//alert("Os bolos da bauduco são divinos")
+//document.writeln("Salve")
+//console.log('batatão')
+
+a = 2
+b = 3
+soma = a+b
+menos = a-b
+mult = a*b
+divi = a/b
+
+nome = 'Antônio'
+cidade = 'Canoas'
+
+//document.writeln("me chamo " +nome+ ", moro em "+cidade)
+//document.writeln("<h1>Aula de Java</h1>")
+
+document.writeln("Adição = "+soma)
+document.writeln("<br>Subtração = "+menos)
+document.writeln("<br>Multiplicação = "+mult)
+document.writeln("<br>Divisão = "+divi)
+
+document.writeln(
+    '<br><BR>Adição: '+soma+
+    '<br>Subtração: '+menos+
+    '<br>Multiplicação: '+multi+
+    '<br>Divisão: '+divi
+)
